226295082b70f7aaf44faf8f0b69f7193b9e4081
theflippinlabs <theflippinlabs@gmail.com>
2026-02-25 07:23:27 +0100

    fix: allow railway host

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'flippinpulse-production.up.railway.app'
+    ]
+  },
+  server: {
+    host: true
+  }
 })
