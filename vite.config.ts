d195458667a798b6909e6b537ba0c22dc65f1503
theflippinlabs <theflippinlabs@gmail.com>
2026-02-25 03:02:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    allowedHosts: true
+  },
   server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+    host: true,
+    port: Number(process.env.PORT) || 5173
+  }
+})
