aba5d934344825ccf367a5666215b8e7f00460c0
theflippinlabs <theflippinlabs@gmail.com>
2026-02-25 03:41:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  // Pour "vite preview" (prod)
   preview: {
     host: true,
     port: Number(process.env.PORT) || 8080,
     strictPort: true,
-    allowedHosts: [
-      'flippinpulse-production.up.railway.app',
-      '.railway.app',
-      '.up.railway.app'
-    ]
+    allowedHosts: ['.railway.app']
+  },
+
+  // Pour "vite" (dev) au cas où
+  server: {
+    host: true,
+    port: Number(process.env.PORT) || 8080,
+    strictPort: true,
+    allowedHosts: ['.railway.app']
   }
 })
